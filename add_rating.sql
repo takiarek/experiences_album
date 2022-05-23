9e5e2120a5514253d42b42ed635a2841c6d2a587
Arek Kita <arkadiusz.kita@lunarlogic.io>
2022-05-23 13:40:42 +0200

    add movies table to database

--- add_rating.sql
+++ add_rating.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS users (
 );
 CREATE SEQUENCE IF NOT EXISTS users_id_sequence;
 ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_id_sequence');
+
+CREATE SEQUENCE IF NOT EXISTS movies_id_sequence;
+CREATE TABLE IF NOT EXISTS movies (
+  id int PRIMARY KEY DEFAULT nextval('movies_id_sequence'),
+  title varchar(30) NOT NULL
+);

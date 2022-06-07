1ef44fb060bb1ef2cfd71646a892c62cc8611a7c
Arek Kita <arkadiusz.kita@lunarlogic.io>
2022-06-07 11:12:18 +0200

    refactor moods ascriptions repository

--- repositories/moods_ascriptions_repository.rb
+++ repositories/moods_ascriptions_repository.rb
@@ -1,13 +1,22 @@
 class MoodsAscriptionsRepository
-  def create(user_id:, movie_id:, moods_ids:)
-    connection = PG.connect(dbname: "experiences_album")
+  TABLE = "moods_ascriptions"
 
+  def initialize(connection: PG.connect(dbname: "experiences_album"))
+    @connection = connection
+  end
+
+  def create(user_id:, movie_id:, moods_ids:)
     values = moods_ids.map do |mood_id|
       "(#{user_id}, #{movie_id}, #{mood_id})"
     end
 
-    sql_string = "INSERT INTO moods_ascriptions (user_id, movie_id, mood_id) VALUES " << values.join(", ")
+    sql_string = "INSERT INTO #{TABLE} (user_id, movie_id, mood_id) VALUES " << values.join(", ")
 
     connection.exec(sql_string)
+
+    connection.close
   end
+
+  private
+  attr_reader :connection
 end
